95d278942efd5a44d590e3eb426ecd3df7bacf14
Jianhua-Wang <jianhua.mert@gmail.com>
2020-07-10 15:41:28 +0800

    aumotically

--- libraryqc.py
+++ libraryqc.py
@@ -3,9 +3,10 @@
 
 import mappy as mp
 import pandas as pd
-import re,tempfile,shutil,os,argparse,subprocess
+import re, tempfile, shutil, os, argparse, subprocess
 from subprocess import call
 from multiprocessing import Pool
+
 
 def print_logo():
     logo = '''
@@ -21,15 +22,23 @@ def print_logo():
     '''
     print(logo)
 
+
 def parseArguments():
-    parser = argparse.ArgumentParser(usage="Librayqc using pattern matching to extract the sgRNA from pair-end sequencing data.",description="python libraryqc.py -i input/file_path.csv -o output -v input/vector.fa -l input/library.csv",)
-    parser.add_argument('-i','--input', type=str, help='csv file contains the sampl name and fq1, fq2 file path of samples',metavar=''),
-    parser.add_argument('-o','--output', type=str, help='directory of output file',metavar=''),
-    parser.add_argument('-v','--vector', type=str, help='the fasta file of your vector, containing two sequences, 5\' and 3\'.',metavar=''),
-    parser.add_argument('-l','--library', type=str, help='csv file of sgRNA you designed',metavar=''),
-    parser.add_argument('-s','--shot', type=int, help='the number of bases upstream and downstream of sgRNA you exptected to match. More shot might reduce the false positive. default=4',default=4,metavar='')
+    parser = argparse.ArgumentParser(
+        usage="Librayqc using pattern matching to extract the sgRNA from pair-end sequencing data.",
+        description="python libraryqc.py -i input/file_path.csv -o output -v input/vector.fa -l input/library.csv", )
+    parser.add_argument('-i', '--input', type=str,
+                        help='csv file contains the sampl name and fq1, fq2 file path of samples', metavar=''),
+    parser.add_argument('-o', '--output', type=str, help='directory of output file', metavar=''),
+    parser.add_argument('-v', '--vector', type=str,
+                        help='the fasta file of your vector, containing two sequences, 5\' and 3\'.', metavar=''),
+    parser.add_argument('-l', '--library', type=str, help='csv file of sgRNA you designed', metavar=''),
+    parser.add_argument('-s', '--shot', type=int,
+                        help='the number of bases upstream and downstream of sgRNA you exptected to match. More shot might reduce the false positive. default=4',
+                        default=4, metavar='')
     args = parser.parse_args()
     return args
+
 
 lib_distribution_header = '''# id: lib_distribution
 # section_name: \'Designed sgRNA library\'
@@ -71,23 +80,25 @@ position_distribution_header = '''# id: position_distribution
 #    title: position distribution llne Plot
 #    ylab: \'bp from 3 end\''''
 
+
 def extract_sgrna_from_single_read(read):
-    read_out = [0,0,0,0]
+    read_out = [0, 0, 0, 0]
     for hit in vector_aligner.map(read[1]):
         match = re.match(r'{}'.format(pattern), mp.revcomp(read[1]))
         if match:
-            read_out = [read[0],match.group(2),read[2][len(match.group(3)):-len(match.group(1))][::-1],read[3],len(match.group(3))]
+            read_out = [read[0], match.group(2), read[2][len(match.group(3)):-len(match.group(1))][::-1], read[3],
+                        len(match.group(3))]
         else:
             read_out = read
     return read_out
 
-def run_pair_end(sample_name,fq1,fq2):
-    sgrna_fq = open(f'{temp_dir}/{sample_name}_sgrna.fq','w')
-    sgrna_txt = open(f'{temp_dir}/{sample_name}_sgrna.txt','w')
 
+def run_pair_end(sample_name, fq1, fq2):
+    sgrna_fq = open(f'{temp_dir}/{sample_name}_sgrna.fq', 'w')
+    sgrna_txt = open(f'{temp_dir}/{sample_name}_sgrna.txt', 'w')
     print(f'Extract sgRNA from {sample_name}')
-    for read1,read2 in zip(mp.fastx_read(fq1,read_comment=True),mp.fastx_read(fq2,read_comment=True)):
-        read1,read2 = extract_sgrna_from_single_read(read1),extract_sgrna_from_single_read(read2)
+    for read1, read2 in zip(mp.fastx_read(fq1, read_comment=True), mp.fastx_read(fq2, read_comment=True)):
+        read1, read2 = extract_sgrna_from_single_read(read1), extract_sgrna_from_single_read(read2)
         if len(read1) == 5:
             sgrna_txt.write(f'{read1[1]}\t{read1[4]}\t{1}\n')
             sgrna_fq.write(f'@{read1[0]}\n{read1[1]}\n+\n{read1[2]}\n')
@@ -106,46 +117,49 @@ def run_pair_end(sample_name,fq1,fq2):
     sgrna_txt.close()
 
     print(f'Count sgRNA quantity {sample_name}')
-    sgrna_df = pd.read_csv(f'{temp_dir}/{sample_name}_sgrna.txt',sep='\t',names=['sgrna','position','lable'])
+    sgrna_df = pd.read_csv(f'{temp_dir}/{sample_name}_sgrna.txt', sep='\t', names=['sgrna', 'position', 'lable'])
     sgrna_count = sgrna_df['sgrna'].value_counts()
     library_df = pd.read_csv(library)
     library_df['count'] = library_df['sequence'].map(sgrna_count)
 
     lib_distribution = pd.Series(data=[len(library_df[library_df['count'].notnull()]),
                                        len(library_df[library_df['count'].isnull()])],
-                                 index=['Found','Not found'])
+                                 index=['Found', 'Not found'])
 
     sgrna_distribution = pd.Series(data=[library_df.drop_duplicates(subset=['sequence'])['count'].sum(),
-                                        sgrna_count.sum()-library_df.drop_duplicates(subset=['sequence'])['count'].sum()],
-                                  index=['Mapped','Unmapped'])
-    
+                                         sgrna_count.sum() - library_df.drop_duplicates(subset=['sequence'])[
+                                             'count'].sum()],
+                                   index=['Mapped', 'Unmapped'])
+
     read_distribution = sgrna_df['lable'].value_counts()
-    read_distribution.index = read_distribution.index.map(pd.Series(index=[1,2,3,4],data=['matched','empty','one end unmapped','pair end unmapped']))
-    
+    read_distribution.index = read_distribution.index.map(
+        pd.Series(index=[1, 2, 3, 4], data=['matched', 'empty', 'one end unmapped', 'pair end unmapped']))
+
     position_distribution = sgrna_df['position'].value_counts()
 
-    sgrna_plot = open(f'{temp_dir}/{sample_name}_sgrna_plot_mqc.txt','w')
-    sgrna_plot.write(f'{sgrna_distribution_header}\n{sgrna_distribution.to_csv(sep=chr(9),header=False)}')
+    sgrna_plot = open(f'{temp_dir}/{sample_name}_sgrna_plot_mqc.txt', 'w')
+    sgrna_plot.write(f'{sgrna_distribution_header}\n{sgrna_distribution.to_csv(sep=chr(9), header=False)}')
     sgrna_plot.close()
-    lib_plot = open(f'{temp_dir}/{sample_name}_lib_plot_mqc.txt','w')
-    lib_plot.write(f'{lib_distribution_header}\n{lib_distribution.to_csv(sep=chr(9),header=False)}')
+    lib_plot = open(f'{temp_dir}/{sample_name}_lib_plot_mqc.txt', 'w')
+    lib_plot.write(f'{lib_distribution_header}\n{lib_distribution.to_csv(sep=chr(9), header=False)}')
     lib_plot.close()
-    read_plot = open(f'{temp_dir}/{sample_name}_read_plot_mqc.txt','w')
-    read_plot.write(f'{read_distribution_header}\n{read_distribution.to_csv(sep=chr(9),header=False)}')
+    read_plot = open(f'{temp_dir}/{sample_name}_read_plot_mqc.txt', 'w')
+    read_plot.write(f'{read_distribution_header}\n{read_distribution.to_csv(sep=chr(9), header=False)}')
     read_plot.close()
-    read_plot = open(f'{temp_dir}/{sample_name}_position_plot_mqc.txt','w')
-    read_plot.write(f'{position_distribution_header}\n{position_distribution.to_csv(sep=chr(9),header=False)}')
+    read_plot = open(f'{temp_dir}/{sample_name}_position_plot_mqc.txt', 'w')
+    read_plot.write(f'{position_distribution_header}\n{position_distribution.to_csv(sep=chr(9), header=False)}')
     read_plot.close()
 
     print(f'Run fastqc on extracted fastq {sample_name}')
     FNULL = open(os.devnull, 'w')
-    call(f'fastqc {temp_dir}/{sample_name}_sgrna.fq -o {temp_dir}',shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+    call(f'fastqc {temp_dir}/{sample_name}_sgrna.fq -o {temp_dir}', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
     shutil.move(f"{temp_dir}/{sample_name}_sgrna.fq", f"{output_dir}/{sample_name}_sgrna.fq")
+
 
 if __name__ == "__main__":
     print_logo()
     args = parseArguments()
-    
+
     output_dir = args.output
     vector = args.vector
     library = args.library
@@ -159,22 +173,23 @@ if __name__ == "__main__":
     fq_path_df = pd.read_csv(fq_path)
 
     vector_sequence = mp.Aligner(vector)
-    with open(f'{temp_dir}/vector1.fa','w') as vector_aligner:
-        vector_aligner.write(f">vector\n{vector_sequence.seq('5')+vector_sequence.seq('3')}\n")
+    with open(f'{temp_dir}/vector1.fa', 'w') as vector_aligner:
+        vector_aligner.write(f">vector\n{vector_sequence.seq('5') + vector_sequence.seq('3')}\n")
     vector_aligner = mp.Aligner(f'{temp_dir}/vector1.fa')
-    pattern = '([ATCG]*'+vector_sequence.seq('5')[-shot:]+')([ACTG]{19,20})('+vector_sequence.seq('3')[:shot]+'[ATCG]*)'
+    pattern = '([ATCG]*' + vector_sequence.seq('5')[-shot:] + ')([ACTG]{19,20})(' + vector_sequence.seq('3')[
+                                                                                    :shot] + '[ATCG]*)'
 
     p = Pool(len(fq_path_df))
     for i in fq_path_df.index:
-        sample_name,fq1,fq2 = fq_path_df.loc[i].values
-        p.apply_async(run_pair_end,(sample_name,fq1,fq2))
+        sample_name, fq1, fq2 = fq_path_df.loc[i].values
+        p.apply_async(run_pair_end, (sample_name, fq1, fq2))
     p.close()
     p.join()
 
     os.chdir(temp_dir)
     print('Generate report')
     FNULL = open(os.devnull, 'w')
-    call(f'multiqc -f --no-data-dir . -c {py_dir}/config.yml',shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+    call(f'multiqc -f --no-data-dir . -c {py_dir}/config.yml', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
     os.chdir(work_dir)
     shutil.move(f"{temp_dir}/multiqc_report.html", f"{output_dir}/multiqc_report.html")
     shutil.rmtree(temp_dir)
